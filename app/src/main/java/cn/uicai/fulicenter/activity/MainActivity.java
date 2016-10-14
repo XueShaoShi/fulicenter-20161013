70ddb03e269d44a7b1752aa0a038ce349ee8c14c
xiaomiao <13227036990@163.com>
2016-10-14 17:00:09 +0800

    添加输出日志类  并在MainActivity中实现

--- app/src/main/java/cn/uicai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/uicai/fulicenter/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.uicai.fulicenter.R;
+import cn.uicai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("Mainactivity onCreate");
     }
 }
