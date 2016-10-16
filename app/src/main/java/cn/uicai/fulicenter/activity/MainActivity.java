11f89bea585ad40b535bf7b124984cf1b359ca35
xiaomiao <13227036990@163.com>
2016-10-16 13:01:23 +0800

    添加底部菜单

--- app/src/main/java/cn/uicai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/uicai/fulicenter/activity/MainActivity.java
@@ -2,6 +2,7 @@ package cn.uicai.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.uicai.fulicenter.R;
 import cn.uicai.fulicenter.utils.L;
@@ -13,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.i("Mainactivity onCreate");
+    }
+
+    public void onCheckedChange(View v){
+        switch (v.getId()) {
+            case R.id.layout_new_good:
+
+                break;
+            case R.id.layout_boutique:
+
+                break;
+            case R.id.layout_category:
+
+                break;
+            case R.id.layout_cart:
+
+                break;
+            case R.id.layout_personal_center:
+
+                break;
+        }
     }
 }
