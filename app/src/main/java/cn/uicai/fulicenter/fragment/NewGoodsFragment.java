420a070f48e08d57695d1f208effc34db047dd9f
xiaomiao <13227036990@163.com>
2016-10-18 17:03:45 +0800

    新品首页的数据以完成（内有BUG 加载提示框不显示）

--- app/src/main/java/cn/uicai/fulicenter/fragment/NewGoodsFragment.java
+++ app/src/main/java/cn/uicai/fulicenter/fragment/NewGoodsFragment.java
@@ -25,6 +25,7 @@ import cn.uicai.fulicenter.net.OkHttpUtils;
 import cn.uicai.fulicenter.utils.CommonUtils;
 import cn.uicai.fulicenter.utils.ConvertUtils;
 import cn.uicai.fulicenter.utils.L;
+import cn.uicai.fulicenter.view.SpaceItemDecoration;
 
 /**
  * Created by xiaomiao on 2016/10/17.
@@ -43,6 +44,7 @@ public class NewGoodsFragment extends Fragment {
     GoodsAdapter mAdapter;
     ArrayList<NewGoodsBean> mList;
     int pageId=1;
+    GridLayoutManager glm;
 
 
 
@@ -56,12 +58,57 @@ public class NewGoodsFragment extends Fragment {
         mAdapter = new GoodsAdapter(mContext,mList);
         initView();
         initData();
+        setListener();
         return layout;
     }
 
+    private void setListener() {
+        setPullUpListener();//上拉加载
+        setPullDownListener();//下拉刷新
+
+    }
+
+    private void setPullUpListener() {
+        mrv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int lastPostion = glm.findFirstCompletelyVisibleItemPosition();//拿到最后一条数据
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && lastPostion == mAdapter.getItemCount() - 1
+                        && mAdapter.isMore()) {
+                    pageId++;
+                    downloadnewGoods(I.ACTION_PULL_UP);
+
+                }
 
 
-    private void initData() {
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int firstPosition = glm.findFirstCompletelyVisibleItemPosition();
+                msrl.setEnabled(firstPosition==0);
+            }
+        });
+
+    }
+
+    private void setPullDownListener() {
+        //下拉刷新的方法
+        msrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                msrl.setRefreshing(true);//显示下拉刷新的提示
+                mtvRfresh.setVisibility(View.VISIBLE);
+                pageId=1;
+                downloadnewGoods(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+        //下载数据
+    private void downloadnewGoods(final int action) {
         NetDao.downloadnewGoods(mContext, pageId, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
             @Override
             public void onSuccess(NewGoodsBean[] result) {
@@ -70,12 +117,17 @@ public class NewGoodsFragment extends Fragment {
                 mAdapter.setMore(true);
                 if (result != null && result.length > 0) {
                     ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
-                    mAdapter.initData(list);
+                    if (action == I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
+                        mAdapter.initData(list);
+                    } else {
+                        mAdapter.addData(list);
+                    }
+
                     if (list.size() < I.PAGE_SIZE_DEFAULT) {
-                        mAdapter.setMore(false);//如果加载的时候小于10  那也不显示
+                        mAdapter.setMore(false);//显示没有更多数据
                     }
                 } else {
-                    mAdapter.setMore(false);//没有获取到数据
+                    mAdapter.setMore(false);//显示没有更多数据
                 }
             }
 
@@ -83,11 +135,20 @@ public class NewGoodsFragment extends Fragment {
             public void onError(String error) {
                 msrl.setRefreshing(false);//设置刷新为False  不在显示
                 mtvRfresh.setVisibility(getView().GONE);//设置提示为不可见
+                mAdapter.setMore(false);
                 CommonUtils.showLongToast(error);
                 L.e("error"+error);
             }
         });
+
     }
+
+
+    private void initData() {
+        downloadnewGoods(I.ACTION_DOWNLOAD);
+    }
+
+
 
     private void initView() {
         msrl.setColorSchemeColors(
@@ -96,10 +157,11 @@ public class NewGoodsFragment extends Fragment {
                 getResources().getColor(R.color.google_red),
                 getResources().getColor(R.color.google_yellow)
         );
-        GridLayoutManager glm = new GridLayoutManager(mContext, I.COLUM_NUM);
+        glm = new GridLayoutManager(mContext, I.COLUM_NUM);
         mrv.setLayoutManager(glm);
         mrv.setHasFixedSize(true);
         mrv.setAdapter(mAdapter);
+        mrv.addItemDecoration(new SpaceItemDecoration(20));
     }
 
 }
--- app/src/main/java/cn/uicai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/uicai/fulicenter/net/NetDao.java
@@ -19,4 +19,5 @@ public class NetDao {
                 .targetClass(NewGoodsBean[].class)
                 .execute(lsitener);
     }
+
 }
