00c95ffd26bfcf876ccd65f4481d8b6e45904867
xiaomiao <13227036990@163.com>
2016-10-19 01:05:11 +0800

    添加商品详情的数据（图片的轮播和简介）

--- app/src/main/java/cn/uicai/fulicenter/activity/GoodsDetailActivity.java
+++ app/src/main/java/cn/uicai/fulicenter/activity/GoodsDetailActivity.java
@@ -1,21 +1,25 @@
 package cn.uicai.fulicenter.activity;
 
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.uicai.fulicenter.I;
 import cn.uicai.fulicenter.R;
+import cn.uicai.fulicenter.bean.AlbumsBean;
 import cn.uicai.fulicenter.bean.GoodsDetailsBean;
 import cn.uicai.fulicenter.net.NetDao;
 import cn.uicai.fulicenter.net.OkHttpUtils;
 import cn.uicai.fulicenter.utils.CommonUtils;
 import cn.uicai.fulicenter.utils.L;
+import cn.uicai.fulicenter.utils.MFGT;
 import cn.uicai.fulicenter.view.FlowIndicator;
+import cn.uicai.fulicenter.view.SlideAutoLoopView;
 
 public class GoodsDetailActivity extends AppCompatActivity {
 
@@ -32,10 +36,12 @@ public class GoodsDetailActivity extends AppCompatActivity {
     TextView tvGoodPriceShop;
     @BindView(R.id.tv_good_price_current)
     TextView tvGoodPriceCurrent;
+    @BindView(R.id.salv)
+    SlideAutoLoopView salv;
     @BindView(R.id.indicator)
     FlowIndicator indicator;
-    @BindView(R.id.salv)
-    SwipeRefreshLayout salv;
+    @BindView(R.id.wv_good_brief)
+    WebView wvGoodBrief;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,9 +89,36 @@ public class GoodsDetailActivity extends AppCompatActivity {
         tvGoodName.setText(details.getGoodsName());
         tvGoodPriceCurrent.setText(details.getCurrencyPrice());
         tvGoodPriceShop.setText(details.getShopPrice());
+        salv.startPlayLoop(indicator, getAlbumImUrl(details), getAlbumImgCount(details));
+        wvGoodBrief.loadDataWithBaseURL(null, details.getGoodsBrief(), I.TEXT_HTML, I.UTF_8, null);
+
+    }
+
+    private int getAlbumImgCount(GoodsDetailsBean details) {
+        if (details.getPromotePrice() != null && details.getPromotePrice().length() > 0) {
+            return details.getProperties()[0].getAlbums().length;
+        }
+        return 0;
+    }
+
+    private String[] getAlbumImUrl(GoodsDetailsBean details) {
+        String[] url = new String[]{};
+        if (details.getPromotePrice() != null && details.getPromotePrice().length() > 0) {
+            AlbumsBean[] albums = details.getProperties()[0].getAlbums();
+            url = new String[albums.length];
+            for (int i = 0; i < albums.length; i++) {
+                url[i] = albums[i].getImgUrl();
+            }
+        }
+        return url;
     }
 
     private void initView() {
 
+    }
+
+    @OnClick(R.id.backClickArea)
+    public void onBackClick() {
+        MFGT.finish(this);
     }
 }
