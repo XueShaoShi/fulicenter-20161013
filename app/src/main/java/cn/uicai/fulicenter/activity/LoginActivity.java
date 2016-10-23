83d024616d509027d7166885a82d0d92b88ae44f
xiaomiao <13227036990@163.com>
2016-10-23 21:51:25 +0800

    把注册成功后的用户名放到登录界面

--- app/src/main/java/cn/uicai/fulicenter/activity/LoginActivity.java
+++ app/src/main/java/cn/uicai/fulicenter/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package cn.uicai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -7,21 +8,21 @@ import android.widget.EditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.uicai.fulicenter.I;
 import cn.uicai.fulicenter.R;
 import cn.uicai.fulicenter.utils.MFGT;
 
 public class LoginActivity extends BaseActivity {
 
     @BindView(R.id.username)
-    EditText username;
+    EditText musername;
     @BindView(R.id.password)
-    EditText password;
+    EditText mpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-
         super.onCreate(savedInstanceState);
     }
 
@@ -48,6 +49,15 @@ public class LoginActivity extends BaseActivity {
             case R.id.btn_register:
                 MFGT.gotoRegister(this);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_REGISTER){
+            String name = data.getStringExtra(I.User.USER_NAME);
+            musername.setText(name);
         }
     }
 }
