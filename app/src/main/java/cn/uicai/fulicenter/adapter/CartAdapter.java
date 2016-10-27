fe988a3452c3be74e4cec5075a5674c4251ef478
xiaomiao <13227036990@163.com>
2016-10-27 20:44:36 +0800

    购物车数量的添加

--- app/src/main/java/cn/uicai/fulicenter/adapter/CartAdapter.java
+++ app/src/main/java/cn/uicai/fulicenter/adapter/CartAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -16,11 +17,16 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.uicai.fulicenter.I;
 import cn.uicai.fulicenter.R;
 import cn.uicai.fulicenter.bean.CartBean;
 import cn.uicai.fulicenter.bean.GoodsDetailsBean;
+import cn.uicai.fulicenter.bean.MessageBean;
+import cn.uicai.fulicenter.net.NetDao;
+import cn.uicai.fulicenter.net.OkHttpUtils;
 import cn.uicai.fulicenter.utils.ImageLoader;
+
 
 /**
  * Created by xiaomiao on 2016/10/19.
@@ -30,43 +36,28 @@ public class CartAdapter extends Adapter<CartAdapter.CartViewHolder> {
     Context mContext;
     ArrayList<CartBean> mList;
 
-    public CartAdapter(Context Context, ArrayList<CartBean> List) {
-        this.mContext = Context;
-        mList = List;
+    public CartAdapter(Context context, ArrayList<CartBean> list) {
+        mContext = context;
+        mList = list;
     }
 
-
-    /**
-     * 绑定布局
-     *
-     * @param parent
-     * @param viewType
-     * @return
-     */
     @Override
     public CartViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        CartViewHolder holder = new CartViewHolder(View.inflate(mContext,
-                R.layout.item_cart, null));
-
+        CartViewHolder holder = new CartViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.item_cart, parent, false));
         return holder;
     }
 
-    /**
-     * 绑定数据
-     *
-     * @param holder
-     * @param position
-     */
     @Override
     public void onBindViewHolder(CartViewHolder holder, int position) {
         final CartBean cartBean = mList.get(position);
         GoodsDetailsBean goods = cartBean.getGoods();
-        if (goods != null) {
+        if(goods!=null) {
             ImageLoader.downloadImg(mContext, holder.mIvCartThumb, goods.getGoodsThumb());
             holder.mTvCartGoodName.setText(goods.getGoodsName());
             holder.mTvCartPrice.setText(goods.getCurrencyPrice());
         }
-        holder.mIvCartCount.setText("(" + cartBean.getCount() + ")");
+        holder.mTvCartCount.setText("("+cartBean.getCount()+")");
         holder.mCbCartSelected.setChecked(false);
         holder.mCbCartSelected.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -75,27 +66,20 @@ public class CartAdapter extends Adapter<CartAdapter.CartViewHolder> {
                 mContext.sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART));
             }
         });
+        holder.mIvCartAdd.setTag(position);
     }
 
-    /**
-     * 获取要显示的总量
-     *
-     * @return
-     */
     @Override
     public int getItemCount() {
-
         return mList != null ? mList.size() : 0;
     }
 
-
     public void initData(ArrayList<CartBean> list) {
         mList = list;
-        notifyDataSetChanged();//刷新
+        notifyDataSetChanged();
     }
 
-
-     class CartViewHolder extends ViewHolder {
+    class CartViewHolder extends ViewHolder {
         @BindView(R.id.cb_cart_selected)
         CheckBox mCbCartSelected;
         @BindView(R.id.iv_cart_thumb)
@@ -104,16 +88,38 @@ public class CartAdapter extends Adapter<CartAdapter.CartViewHolder> {
         TextView mTvCartGoodName;
         @BindView(R.id.iv_cart_add)
         ImageView mIvCartAdd;
-        @BindView(R.id.iv_cart_count)
-        TextView mIvCartCount;
+        @BindView(R.id.tv_cart_count)
+        TextView mTvCartCount;
         @BindView(R.id.iv_cart_del)
         ImageView mIvCartDel;
         @BindView(R.id.tv_cart_price)
         TextView mTvCartPrice;
 
-         CartViewHolder(View view) {
+        CartViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
+        }
+
+        @OnClick(R.id.iv_cart_add)
+        public void addCart(){
+            final int position = (int) mIvCartAdd.getTag();
+            CartBean cart = mList.get(position);
+            NetDao.updateCart(mContext, cart.getId(), cart.getCount() + 1, new
+                    OkHttpUtils.OnCompleteListener<MessageBean>() {
+                @Override
+                public void onSuccess(MessageBean result) {
+                    if(result!=null && result.isSuccess()){
+                        mList.get(position).setCount(mList.get(position).getCount()+1);
+                        mContext.sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART));
+                        mTvCartCount.setText("("+(mList.get(position).getCount())+")");
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+
+                }
+            });
         }
     }
 }
